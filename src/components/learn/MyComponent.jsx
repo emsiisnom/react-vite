762c8a71e6466842a8ed7d8f063611997314284a
HoangVy <hoangvy09102003@gmail.com>
2026-05-11 13:43:16 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,12 @@
 //component = html + css + js
 const MyComponent = () => {
   return (
-    <div>Hello World update</div>
+    <>
+      <div>Hello World update</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
   );
-}
-
+};
 export default MyComponent;
