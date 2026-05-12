ddc95c8f5e373061f20a6991281953aef97ac864
HoangVy <hoangvy09102003@gmail.com>
2026-05-12 14:34:41 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,10 +2,22 @@ const TodoNew = (props) => {
   const { addNewTodo } = props;
 
   // addNewTodo();
+  const handClick = () => {
+    alert("click me");
+  };
+  const handleOnChange = (name) => {
+    console.log("handleOnChange", name);
+  };
+
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        onChange={(event) => handleOnChange(event.target.value)}
+      />
+      <button style={{ cursor: "pointer" }} onClick={handClick}>
+        Add
+      </button>
     </div>
   );
 };
