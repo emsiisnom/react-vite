295187af0f6d105a54d22047931a62e672221966
HoangVy <hoangvy09102003@gmail.com>
2026-05-12 12:57:42 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,11 @@
 //component = html + css + js
 const MyComponent = () => {
+  const string1 = "string"; //string
+  const number = 25; //number
   return (
     <>
-      <div>Hello World update</div>
+      <div>{string1} Hello World update</div>
+      <div>{console.log("test")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
